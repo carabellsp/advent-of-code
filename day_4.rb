0b19a9a627c1bea06a48093b2d993e231a6e166a
carabellsp <cara.bell@smartpension.co.uk>
2021-12-09 10:45:20 +0000

    add explanatory comments to day4 part A

--- day_4.rb
+++ day_4.rb
@@ -6,10 +6,14 @@ def read_input
 end
 
 def cards
+  # we exclude line[0] as it contains the numbers not the cards
+  # we then map the numbers into arrays of 5 integers
+  # then using each_slice, we group 5 arrays into a larger array - these are our cards
   read_input[1..-1].map { |a| a.split(/\s+/).map(&:to_i) }.each_slice(5).flat_map { |a| [a, a.transpose] }
 end
 
 def numbers
+  # creates an array of the numbers in the order they will be called
   read_input.first.split(',').map(&:to_i)
 end
 
@@ -18,8 +22,11 @@ def bingo_winner(numbers, cards)
     selected = numbers[0..index]
     cards.each do |card|
       card.each do |row|
+        # we use next in an iterative cycle to move on to the next item if the condition is not met
+        # i.e. if we do not have 5 bingo numbers in a row
         next unless (row & selected).length == 5
 
+        # we multiply the most recent number by the remaining card elements sum
         puts n * (card.flatten - selected).sum
         exit
       end
